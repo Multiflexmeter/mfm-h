bc9b334bc692209d0d3ce719f7d6be160211a292
Tim van Osch <timvosch@pollex.nl>
2019-04-15 14:09:30 +0200

    Remove comments

--- src/main.cpp
+++ src/main.cpp
@@ -4,25 +4,12 @@
 // ======= JOBS ======
 osjob_t sendJob;
 
-/**
- * ==================
- * BEGIN EDIT THIS
- * ==================
- */
 const int SEND_INTERVAL_SECONDS = 60;
-// This value will be sent to Grafana
+
 int getMeasurement()
 {
-  // int value = analogRead(A0);
-  // int mv = (int)((float)value/1024*5000);
-  // return mv;
   return 64;
 }
-/**
- * ==================
- *    	END
- * ==================
- */
 
 void sendFunc(osjob_t *j)
 {
@@ -49,6 +36,7 @@ void setup()
   Serial.begin(9600);
   MFMLora::setup();
 }
+
 void loop()
 {
   os_runloop_once();
