8690a80c96fe1377e8383a6ac48f9629034c4fb3
Tim van Osch <timvosch@pollex.nl>
2019-05-03 20:17:15 +0200

    Adjust for DS18B20 sensor array

--- src/main.cpp
+++ src/main.cpp
@@ -1,81 +1,66 @@
 #include <Arduino.h>
 #include <DallasTemperature.h>
-#include <SoftwareSerial.h>
 #include <MFMLora.h>
 
 /*
   Instrument configuration
 */
+// Length of sensor array
+#define SENSOR_COUNT 17
 // pins
 #define RELAY_PIN 2
-#define JSNV2_TX 4
-#define JSNV2_RX 3
-#define ONEWIRE_PIN 5
-// settings
-#define WARMUP_TIME_MS 200
-#define JSNV2_TIMEOUT_MS 100
+#define WARMUP_TIME_MS 100
+#define ONEWIRE_PIN 6
 
 OneWire oneWire(ONEWIRE_PIN);
 DallasTemperature sensors(&oneWire);
-SoftwareSerial JSNV2(JSNV2_TX, JSNV2_RX);
+
+// DS18B20 sensors in order (TOP -> BOTTOM)
+// We only need bytes 3 through 8
+// Technically we can calculate the CRC (8th) byte
+const unsigned char SENSORS_ADDRESSES[SENSOR_COUNT * 6] = {
+  0x75, 0x34, 0x2E, 0x16, 0x01, 0xA5,
+  0xAF, 0xC0, 0x2C, 0x16, 0x02, 0x94,
+  0xDF, 0xB2, 0x2C, 0x16, 0x02, 0x56,
+  0x73, 0xC8, 0x2C, 0x16, 0x02, 0xF7,
+  0xDE, 0x8E, 0x2E, 0x16, 0x01, 0x6C,
+  0x13, 0x3C, 0x2E, 0x16, 0x01, 0x34,
+  0x3F, 0x11, 0x2E, 0x16, 0x01, 0x00,
+  0xE4, 0x26, 0x2E, 0x16, 0x01, 0xE0,
+  0xD6, 0xB4, 0x2C, 0x16, 0x02, 0xAC,
+  0xF1, 0x8B, 0x2E, 0x16, 0x01, 0x7A,
+  0x12, 0x8F, 0x2E, 0x16, 0x01, 0xE0,
+  0x51, 0xC6, 0x2C, 0x16, 0x02, 0x99,
+  0x79, 0xC6, 0x2C, 0x16, 0x02, 0x5F,
+  0xBD, 0x62, 0x2C, 0x16, 0x02, 0xC5,
+  0x78, 0xB2, 0x2C, 0x16, 0x02, 0x34,
+  0x00, 0xB5, 0x2C, 0x16, 0x02, 0xE1,
+  0x19, 0x61, 0x2C, 0x16, 0x02, 0x61
+};
 
 struct Packet
 {
-  unsigned short distance;
-  float temperature;
+  float temperature[SENSOR_COUNT];
 };
-
-unsigned short getDistance(void)
-{
-  uint8_t buffer[4];
-  unsigned long start;
-
-  // send "Start Reading" command to JSNV2
-  JSNV2.write(0x55);
-  Serial.flush();
-  // Wait until there is data available
-  start = millis();
-  while (!(JSNV2.available() > 0))
-  {
-    delay(10);
-    if (start + JSNV2_TIMEOUT_MS < millis())
-    {
-      return -1;
-    }
-  }
-  // There is data availabe, start parsing it.
-  // Read four bytes
-  JSNV2.readBytes(buffer, 4);
-  // Verify checksum
-  if (buffer[0] == 0xff && ((buffer[0] + buffer[1] + buffer[2]) & 0x00ff) == buffer[3])
-  {
-    // Calculate distance
-    unsigned short dist = (buffer[1] << 8) | buffer[2]; //distance
-    return float(dist) / 10.0f;
-  }
-
-  // The packet was invalid.
-  return -2;
-}
-
-float getTemperature(void)
-{
-  sensors.begin();
-  sensors.requestTemperatures();
-  return sensors.getTempCByIndex(0);
-}
 
 /**
  * Fired when data is to be collected.
  * 
- * Collected data must be copied to `LMIC.frame`
+ * Collected data must be copied to `MFMLora::txData`
  */
 void doMeasurements(osjob_t *j)
 {
-  Packet packet{
-      getDistance(), getTemperature()};
-
-  // Temporary data preparation
+  Packet packet;
+  for (unsigned char i = 0; i < SENSOR_COUNT; i++)
+  {
+    // Prepend address with manufacturer + id
+    unsigned char address[8] = {0x28, 0xEE};
+    // Copy serial + CRC Byte at a 2 byte offset
+    memcpy(&address[i], &SENSORS_ADDRESSES[i * 6] + 0x2, 6);
+    // Read and place temperature in packet
+    packet.temperature[i] = sensors.getTempC(address);
+  }
+  // Copy packet struct to txData
   memcpy(MFMLora::txData, (void *)&packet, sizeof(packet));
   MFMLora::txDataLen = sizeof(packet);
 }
@@ -88,9 +73,10 @@ void doMeasurements(osjob_t *j)
 void powerUp(osjob_t *j)
 {
   digitalWrite(RELAY_PIN, HIGH);
-  JSNV2.begin(9600);
-  // Wait for the JSN to come online
+  // Wait for instruments to come online
   delay(WARMUP_TIME_MS);
+  // Start DS 1-wire bus
+  sensors.begin();
 }
 
 /**
@@ -100,7 +86,6 @@ void powerUp(osjob_t *j)
  */
 void powerDown(osjob_t *j)
 {
-  JSNV2.end();
   digitalWrite(RELAY_PIN, LOW);
 }
 
