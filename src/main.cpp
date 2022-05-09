c1600b304ec128972d93e66b5e6f03f07424aeae
Tim van Osch <timvosch@pollex.nl>
2022-05-09 14:37:09 +0200

    Increase clock correction for m328p

--- src/main.cpp
+++ src/main.cpp
@@ -57,17 +57,7 @@ void setup(void)
 
   os_init();
   LMIC_reset();
-
-  LMIC_setClockError(MAX_CLOCK_ERROR * 2 / 100);
-  LMIC_setupChannel(0, 868100000, DR_RANGE_MAP(DR_SF12, DR_SF7), BAND_CENTI);  // g-band
-  LMIC_setupChannel(1, 868300000, DR_RANGE_MAP(DR_SF12, DR_SF7B), BAND_CENTI); // g-band
-  LMIC_setupChannel(2, 868500000, DR_RANGE_MAP(DR_SF12, DR_SF7), BAND_CENTI);  // g-band
-  LMIC_setupChannel(3, 867100000, DR_RANGE_MAP(DR_SF12, DR_SF7), BAND_CENTI);  // g-band
-  LMIC_setupChannel(4, 867300000, DR_RANGE_MAP(DR_SF12, DR_SF7), BAND_CENTI);  // g-band
-  LMIC_setupChannel(5, 867500000, DR_RANGE_MAP(DR_SF12, DR_SF7), BAND_CENTI);  // g-band
-  LMIC_setupChannel(6, 867700000, DR_RANGE_MAP(DR_SF12, DR_SF7), BAND_CENTI);  // g-band
-  LMIC_setupChannel(7, 867900000, DR_RANGE_MAP(DR_SF12, DR_SF7), BAND_CENTI);  // g-band
-  LMIC_setupChannel(8, 868800000, DR_RANGE_MAP(DR_FSK, DR_FSK), BAND_MILLI);   // g2-band
+  LMIC_setClockError(MAX_CLOCK_ERROR * 5 / 100);
 
   LMIC_startJoining();
 }
@@ -153,6 +143,7 @@ void onEvent(ev_t ev)
   case EV_JOINING:
     _debugTime();
     _debug(F("EV_JOINING\n"));
+    LMIC_setDrTxpow(DR_SF10, 14);
     os_clearCallback(&job);
     break;
 
